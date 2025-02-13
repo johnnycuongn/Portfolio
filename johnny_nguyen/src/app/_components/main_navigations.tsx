c43b62f12355b88b6a3a17d24c698032bda16326
Johnny Nguyen <cuongdn2001@gmail.com>
2025-02-13 11:57:32 +1100

    feat: Update background color and navigation style

--- johnny_nguyen/src/app/_components/main_navigations.tsx
+++ johnny_nguyen/src/app/_components/main_navigations.tsx
@@ -1,7 +1,7 @@
-const NavBarAClassName = "text-white hover:text-gray-300 pointer-events-auto px-4 py-2";
+const NavBarAClassName = "text-white rounded  pointer-events-auto p-2";
 
 function PortfolioNavBar() {
-  return <nav className="flex flex-row justify-end bg-blue-400">
+  return <nav className="fixed top-0 right-0 flex flex-row justify-end p-2 gap-2 z-100">
       <a className={NavBarAClassName}>Home</a>
       <a className={NavBarAClassName}>Projects</a>
       <a className={NavBarAClassName}>Resume</a>
--- johnny_nguyen/src/app/page.tsx
+++ johnny_nguyen/src/app/page.tsx
@@ -2,7 +2,15 @@
 
 import { useRef, useState, useEffect } from "react";
 import { PortfolioNavBar } from "./_components/main_navigations";
+import { FaGithub, FaLinkedin, FaGoodreads } from "react-icons/fa";
+
 import { motion, useScroll, useTransform, useSpring, useMotionValue } from "motion/react";
+
+const PORTFOLIO = {
+  name: "Duc Cuong Nguyen",
+  role: "Frontend Software Engineer",
+  description: "A full-stack software engineer with 2+ years of experience, who focuses on providing the most high-quality and usable solutions, specialising in React and Typescript. Aimed to deliver projects on time while implementing software engineering practices in the team. Seeking an opportunity for growth, leveraging my software development passion to contribute to high-impact projects."
+}
 
 const ScrollSections = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -13,22 +21,20 @@ const ScrollSections = () => {
   const [section2Position, setSection2Position] = useState({ x: 0, y: 0 });
   
   const [section3Position, setSection3Position] = useState({ x: 0, y: 0 });
-  const { scrollY } = useScroll();
 
-  const nameX = useTransform(scrollY, [0, 300], [0, -100]); // Move name to the left
-  const newTextX = useTransform(scrollY, [0, 300], [100, 0]); // Move new text from right to center
-  const newTextOpacity = useTransform(scrollY, [0, 300], [0, 1]); // Fade in new text
+  const { scrollY } = useScroll();
 
   const y2 = useTransform(scrollY, [0, containerHeight], [0, -containerHeight]);
   const customY2 = useMotionValue(0);
   const springY2 = useSpring(customY2, { damping: 30, stiffness: 80 });
 
-  const y3 = useTransform(scrollY, [containerHeight*1.2, containerHeight * 2.2], [0, -containerHeight]);
+  const y3 = useTransform(scrollY, [containerHeight, containerHeight*2], [0, -containerHeight]);
   const customY3 = useMotionValue(0);
   const springY3 = useSpring(customY3, { damping: 30, stiffness: 80 });
 
   // Text animations
   const text1Opacity = useTransform(scrollY, [0, containerHeight / 2], [1, 0]);
+  const text1Y = useTransform(scrollY, [0, containerHeight / 2], [0, -50]);
 
   const text2Opacity = useTransform(scrollY, [containerHeight / 2, containerHeight, containerHeight * 1.5], [0, 1, 0]);
   const text2Y = useTransform(scrollY, [containerHeight / 2, containerHeight], [-100, 0]);
@@ -36,12 +42,16 @@ const ScrollSections = () => {
   const text3Opacity = useTransform(scrollY, [containerHeight * 1.5, containerHeight * 2], [0, 1]);
   const text3Y = useTransform(scrollY, [containerHeight * 1.5, containerHeight * 2], [50, 0]);
 
+  // Implement magnetic scroll for section 2
   useEffect(() => {
-
     return y2.on("change", (latest: number) => {
-      if (latest > -containerHeight + 150) {
+      if (latest > - 150) {
+        customY2.set(0);
+      }
+      else if (latest > -containerHeight + 150) {
         // Regular scroll behavior when not close to snapping point
         customY2.set(latest);
+       
       } else if (latest <= -containerHeight + 150) {
         // Snap to fully in view when very close
         customY2.set(-containerHeight);
@@ -50,10 +60,14 @@ const ScrollSections = () => {
 
   }, [y2, customY2, springY2, containerHeight]);
   
+  // Implement magnetic scroll for section 3
   useEffect(() => {
 
     return y3.on("change", (latest: number) => {
-      if (latest > -containerHeight + 150) {
+      if (latest > - 150) {
+        customY3.set(0);
+      }
+      else if (latest > -containerHeight + 150) {
         // Regular scroll behavior when not close to snapping point
         customY3.set(latest);
       } else if (latest <= -containerHeight + 150) {
@@ -69,18 +83,6 @@ const ScrollSections = () => {
       setContainerHeight( window.innerHeight);
     }
   }, []);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      if (scrollPosition > containerHeight * 2) {
-        window.scrollTo(0, containerHeight * 2);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [containerHeight]);
 
   useEffect(() => {
     let animationFrameId;
@@ -106,76 +108,69 @@ const ScrollSections = () => {
   }, []);
 
   return (
-    <div ref={containerRef} className="h-[300vh]">
-      <div ref={section1Ref} className="fixed top-0 left-0 w-full h-screen bg-blue-500 flex items-center justify-center text-white text-4xl z-10">
-        
-        <motion.div style={{ opacity: text1Opacity }}>
-        <div className="flex items-center justify-between w-full px-20">
-          <motion.div style={{ x: nameX }} className="text-4xl">
-            Duc Cuong Nguyen
-          </motion.div>
-          <motion.div 
-            style={{ x: newTextX, opacity: newTextOpacity }} 
-            className="text-2xl"
-          >
-            Full Stack Developer
+    <div ref={containerRef} className="h-[300vh] w-max-screen items-center justify-center bg-slate-800">
+      <PortfolioNavBar />
+      {/* Section 1 */}
+      <div ref={section1Ref} className="fixed top-0 left-0 w-full h-screen grid grid-cols-5 items-center justify-center p-4 text-white z-10">
+        <div className="col-span-2 flex flex-col h-full justify-center py-16">
+          <motion.div style={{ opacity: text1Opacity, y: text1Y }}>
+              <h1 className="text-6xl pb-4">{PORTFOLIO.name}</h1>
+              <div className="text-xl pb-4">{PORTFOLIO.role}</div>
+              <div className="flex flex-row gap-4">
+                <a href="https://github.com/johnnycuongn">
+                  <FaGithub size={'25'} />
+                </a>
+                <a href="https://www.linkedin.com/in/johnny-nguyen-2001jcn/">
+                  <FaLinkedin size={'25'} />
+                </a>
+                <a href="">
+                  <FaGoodreads size={'25'} />
+                </a>
+              </div>
           </motion.div>
         </div>
-        <div className="absolute bottom-20 left-0 w-full px-20">
-          <div className="grid grid-cols-6 gap-4">
-            <span className="text-sm">React</span>
-            <span className="text-sm">React</span>
-            <span className="text-sm">React</span>
-            <span className="text-sm">React</span>
-            <span className="text-sm">React</span>
-          </div>
+        <div className="col-span-3 flex flex-col f-full px-4">
+          <motion.div style={{ opacity: text1Opacity, y: text1Y }}>
+            <p>{PORTFOLIO.description}</p>
+          </motion.div>
         </div>
-        </motion.div>
       </div>
+      {/* Section 2 */}
       <motion.div
-        style={{ y: springY2 }}
-        className="fixed top-full left-0 w-full h-screen bg-red-200 z-20"
+        style={{ y: y2 }}
+        className="fixed top-full left-0 w-full h-full p-4 z-20"
       >
-        <div ref={section2Ref} className="h-screen bg-green-500 flex items-center justify-center text-white text-4xl">
+        <div ref={section2Ref} className="h-full flex flex-col text-white">
           <motion.div style={{ opacity: text2Opacity, y: text2Y }}>
-            Section 2 (Scrollable)<br />
-            X: {Math.round(section2Position.x)}, Y: {Math.round(section2Position.y)}
+            <span className="text-4xl">Experience</span>
           </motion.div>
         </div>
       </motion.div>
+
+      {/* Section 3 */}
       <motion.div
-        style={{ y: springY3 }}
-        transition={{ duration: 0.5 }}
-        className="fixed top-full left-0 w-full h-screen bg-red-200 z-30"
+        style={{ y: y3 }}
+        className="fixed top-full left-0 w-full h-full p-4 z-30"
       >
-        <div ref={section3Ref} className="h-screen bg-red-500 flex items-center justify-center text-white text-4xl">
+        <div ref={section3Ref} className="h-screen flex flex-col text-white">
           <motion.div style={{ opacity: text3Opacity, y: text3Y }}>
-            Section 3 (Scrollable)<br />
-            X: {Math.round(section3Position.x)}, Y: {Math.round(section3Position.y)}
+            <span className="text-4xl">Projects</span>
           </motion.div>
         
         </div>
       </motion.div>
     </div>
+
   );
 };
 
 export default function Home() {
   return (
     <div className="">
-      <PortfolioNavBar />
       <main className="">
-        {/* <div className="grid grid-cols-3 min-h-screen bg-blue-400">
-          <div className="bg-red-100 flex flex-col items-center p-4">
-            <p className="text-3xl">Duc Cuong Nguyen</p>
-            <p>A full-stack software engineer with 2+ years of experience, who focuses on providing the most high-quality and usable solutions, specialising in React and Typescript. Aimed to deliver projects on time while implementing software engineering practices in the team. Seeking an opportunity for growth, leveraging my software development passion to contribute to high-impact projects.</p>
-          </div>
-          <div className="col-span-2 bg-gray-200"></div>
-        </div> */}
         <ScrollSections />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
+   
     </div>
   );
 }
