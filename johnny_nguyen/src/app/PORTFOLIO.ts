d26a09a2af10733cb425bdba784c9183ef4dddd1
Johnny Nguyen <cuongdn2001@gmail.com>
2025-02-13 19:37:09 +1100

    feat: Update Portfolio and Timeline

--- johnny_nguyen/src/app/PORTFOLIO.ts
+++ johnny_nguyen/src/app/PORTFOLIO.ts
@@ -6,11 +6,11 @@ interface JobTimeLineItem {
   link: string;
   stacks: string[];
 }
-const TimelineData: JobTimeLineItem[] = [
+const JobTimelineData: JobTimeLineItem[] = [
   {
     year: 'Nov 2024 - Feb 2025', 
     title: 'Software Engineer Intern', 
-    content: "sdfuhasldskdfjlsdfjsldkfjlsdkfjlsdjfkslkdjflsdjflsjdflskdjflsjdfklsdjflsdkfjlsdkfjsldfjslkdfjsldkfjsldkfjsldkfjslkdjfslkdfjlskfjjkhaslkdfhaklsdhfkjlasdhfklasjdhfklajsdhflkajsdhflkasjdhlaksjdhlkasjdhflaksjdhlaksjdhlaksjdfhalksdjhaklsdf",
+    content: "Managed and built License Management System from the ground up, which is used to manage Sharepoint Licenses for over 10 clients.",
     company: 'WebVine',
     link: 'https://webvine.com.au/',
     stacks: ['React', 'Next.js', 'Typescript', 'TailwindCSS', '.NET Core', 'Sharepoint SPFx', 'Azure'],
@@ -18,20 +18,20 @@ const TimelineData: JobTimeLineItem[] = [
   {
     year: 'Feb - Oct 2023',
     title: 'Junior Software Engineer',
-    company: 'Orefox',
-    content: 'skldjflskdjflksdjflksdjf;lsdjfgs;ldfvns;ldfgkh;sdlfgk;sdfjg;lsdkfjg;sdkfjg;sldfkj;lsdgfj',
-    link: '',
-    stacks: ['React', 'Typescript', 'jQuery', 'Django', 'PostgreSQL'],
+    company: 'Orefox AI',
+    content: 'Worked closely with senior engineers to improve current Orefox GeoDesk\' platforms and new apps. Responisble for advanced features inlcuding GeoDesk Scrum Board, Geological Map, Marketplace Platform, and Geologist Chat Platform',
+    link: 'https://orefox.com/',
+    stacks: ['React', 'Typescript', 'jQuery', 'Django', 'PostgreSQL', 'GeoDjango'],
   },
   {
     year: 'Mar - Dec 2022',
     title: 'Software Engineer',
     company: 'Queensland Murray Darling Catchment',
     link: 'https://qmdcl.org.au/',
-    content: '',
+    content: 'Led the development of Water Quality Monitoring platforms, built a new mobile app for river rangers to collect water data in offline mode, and improved the existing web app for data visualization.',
     stacks: ['React', 'React Native','Typescript', 'Node.js', 'Firebase']
     }
 ];
 
-export { TimelineData };
+export { JobTimelineData as TimelineData };
 export type { JobTimeLineItem };
--- johnny_nguyen/src/app/_components/Timeline.tsx
+++ johnny_nguyen/src/app/_components/Timeline.tsx
@@ -1,16 +1,69 @@
 'use client'
 
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
+import { FiExternalLink } from "react-icons/fi";
 import './Timeline.css';
 import { JobTimeLineItem, TimelineData } from '../PORTFOLIO';
+import { motion, useAnimation } from 'motion/react';
 
-const TimelineItem: FC<JobTimeLineItem> = ({ year, title, company, content, link, stacks }) => (
+const useDelayedNavigation = (delay: number) => {
+  const [navigating, setNavigating] = useState(false);
+  const [, setDestination] = useState('');
+
+  const navigate = (url: string) => {
+    setNavigating(true);
+    setDestination(url);
+    setTimeout(() => {
+      window.open(url, '_blank', 'noopener,noreferrer');
+      setNavigating(false);
+    }, delay);
+  };
+
+  return { navigating, navigate };
+};
+
+const TimelineItem: FC<JobTimeLineItem> = ({ year, title, company, content, link, stacks }) => {
+  const controls = useAnimation();
+  const { navigating, navigate } = useDelayedNavigation(200); // 500ms delay
+
+  const handleClick = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (navigating) return;
+
+    await controls.start({
+      x: '100%',
+      y: '-100%',
+      opacity: 0,
+      transition: { duration: 0.5 }
+    });
+    navigate(link);
+    setTimeout(async () => {
+      await controls.start({
+        x: '0',
+        y: '0',
+        opacity: 1,
+        transition: { duration: 0.5 }
+      });
+    }, 2000)
+  };
+  
+  return (
   <li className='timeline-item'>
-    <div className='item flex flex-col h-full gap-1 group transition-all lg:hover:!opacity-100 lg:group-hover:group-[.hovered]:opacity-50'>
-      <a href={link} className="absolute inset-0" aria-label={`Link to ${title} project`}></a>
+    <div className='item flex flex-col h-full gap-1 group/item transition-all lg:hover:!opacity-100 lg:group-hover:group-[.hovered]:opacity-50 lg:hover:shadow-lg'>
+    <a href={link} onClick={handleClick} className="absolute inset-0" aria-label={`Link to ${title} project`}></a>
       <span className='text-sm text-gray-400'>{year}</span>
-      <span className='title'>{title} • {company}</span>
-      <div className='break-all flex-grow text-sm text-gray-400'>
+     
+      <span className='title inline-flex items-center transition-all group-hover/item:text-teal-300'>
+          {title} • {company}
+          <motion.span 
+            className='inline-block ml-2'
+            animate={controls}
+            initial={{ x: 0, y: 0, opacity: 1 }}
+          >
+            <FiExternalLink />
+          </motion.span>
+      </span>
+      <div className='break-words transition-all flex-grow text-sm text-gray-400 group-hover/item:text-white'>
         {content}
       </div>
       <div className='flex flex-wrap gap-1'>
@@ -22,7 +75,7 @@ const TimelineItem: FC<JobTimeLineItem> = ({ year, title, company, content, link
       </div>
     </div>
   </li>
-);
+)};
 
 const Timeline: FC = () => {
   const timelineRef = useRef<HTMLElement>(null);
