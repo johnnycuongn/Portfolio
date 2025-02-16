c0bf22eb3db69dac0dbd7414c49952316a618a45
Johnny Nguyen <cuongdn2001@gmail.com>
2025-02-16 11:45:22 +1100

    feat: udpate animation of profile links

--- johnny_nguyen/src/app/_components/ProfilesLinkGroup.tsx
+++ johnny_nguyen/src/app/_components/ProfilesLinkGroup.tsx
@@ -2,6 +2,7 @@ import { motion, Reorder, useAnimation } from "motion/react";
 import { FC, JSX, memo, useEffect, useState } from "react";
 import { FaGithub, FaLinkedin, FaGoodreads } from "react-icons/fa";
 import { PROFILE_LINKS } from "../PORTFOLIO";
+import wait from "@/utils/wait";
 
 interface PublicProfile {
   title: string;
@@ -17,17 +18,21 @@ const iconMap: { [key: string]: JSX.Element } = {
   Goodreads: <FaGoodreads size={'25'}/>
 };
 
-const ProfileLinkGroupItem: FC<ProfileLinkGroupItemProps> = ({ title }) => {
+const ProfileLinkGroupItem: FC<ProfileLinkGroupItemProps> = ({ index, title }) => {
 
   const controls = useAnimation();
 
   useEffect(() => {
+    if (!controls) return;
+
     const interval = setInterval(() => {
-      controls.start({
-        x: [0, -8, 0],
-        transition: { duration: 1 }
-      });
-    }, 30000);
+      wait((index * 0.5 * 1000)).then(() => {
+        controls.start({
+          y: [0, -8, 0],
+          transition: { duration: 1 }
+        })
+      })
+    }, 8000)
 
     return () => clearInterval(interval);
   }, []);
@@ -45,7 +50,7 @@ const ProfileLinkGroupItem: FC<ProfileLinkGroupItemProps> = ({ title }) => {
         className="bg-slate-900 text-white rounded cursor-grab active:cursor-grabbing select-none flex items-center gap-2 group"
         animate={controls}
         whileTap={{ scale: 0.95 }}
-        title="Drag to reorder tabs"
+        title="Drag to reorder my profiles"
       >
         <span onClick={() => handleItemClick(title)}>
           {iconMap[title]}
@@ -59,20 +64,6 @@ ProfileLinkGroupItem.displayName = "ProfileLinkGroupItem";
 
 function PublicProfilesBar({items}: { items: PublicProfile[]}) {
   const [tabs, setTabs] = useState(items.map(item => item.title));
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setTabs(prevTabs => {
-        if (prevTabs.length < 2) return prevTabs;
-        const newTabs = [...prevTabs];
-        // Swap the first two items
-        [newTabs[0], newTabs[1]] = [newTabs[1], newTabs[0]];
-        return newTabs;
-      });
-    }, 2000);
-
-    return () => clearInterval(timeout);
-  }, []);
 
   return (
     <Reorder.Group 
